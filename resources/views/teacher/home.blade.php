e3af6677c65eec8f605a4c732ac5da445dd7f530
rubickking04 <rubickking04@gmail.com>
2022-05-05 21:39:26 +0800

    first commit

--- resources/views/teacher/home.blade.php
+++ resources/views/teacher/home.blade.php
@@ -6,17 +6,23 @@
         @if(count($subject)>0)
         @foreach ($subject as $subjects)
         <div class="col-lg-3 col-md-6 col-sm-6 mb-3">
-            <div class="card shadow h-100 py-3" style="border-radius:10px;">
+            <div class="card shadow h-100" style="border-radius:20px;">
                 <div class="card-body">
                     <div class="row">
                         <div class="col-lg-8 col-md-8 col-sm-8 col-8">
                             <a href="{{ route('teacher.class',$subjects->uuid) }}" class="text-start d-block text-truncate h4 fw-bolder text-dark card-title" {{ Popper::delay(500,0)->arrow('round')->pop( $subjects->subject.' - '.$subjects->description); }}>{{ $subjects->subject.' - '.$subjects->description }}</a>
                             <h5 class="text-start fw-bold card-text text-decoration-underline" >{{ 'Section - '.$subjects->section }}</h5>
                             <h5 class="text-start card-text py-3">{{ $subjects->teacher->name }}</h5>
-                            {{-- <p>{{ $subjects->uuid }}</p> --}}
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-4 col-4">
                             <img src="{{ asset('/storage/images/avatar.png')}}" class="border border-info border-3 rounded-circle img-thumbnail" alt="" height="100px" width="100px">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-end px-3">
+                        <div class="hstack gap-3">
+                            <a href="{{ route('teacher.class.destroy',$subjects->id) }}" onclick="return confirm('Are you sure to unenroll to this subject?')" class="text-danger text-end" ><i class="fa-solid fa-trash-can fs-3" {{ Popper::delay(500,0)->arrow('round')->pop('Unenroll'); }}></i></a>
                         </div>
                     </div>
                 </div>
