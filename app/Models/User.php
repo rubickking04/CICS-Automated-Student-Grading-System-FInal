12822ac1142fd9021ec984b5bda5a72125f1cf7a
rubickking04 <rubickking04@gmail.com>
2022-05-17 13:59:45 +0800

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,8 @@ class User extends Authenticatable
         'gender',
         'phone',
         'student_type',
+        'curriculum_year',
+        'section',
         'student_status',
         'birth_date',
         'password',
