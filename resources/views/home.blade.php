b6c290ab04ec4940b2c76f6ab49829bcfd64de3d
rubickking04 <rubickking04@gmail.com>
2022-05-26 21:36:03 +0800

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,21 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row ">
+        <div class="row no-gutters">
             @if(count($lesson)>0)
             @foreach ($lesson as $lessons)
             <div class="col-lg-3 col-md-6 col-sm-6 mb-3">
                 <div class="card shadow h-100" style="border-radius:20px;">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-lg-8 col-md-8 col-sm-8 col-8">
+                            <div class="col-lg-9 col-md-9 col-sm-9 col-9">
                                 <a href="{{ route('my-class',$lessons->uuid) }}" class="text-start d-block text-truncate h4 fw-bolder text-dark card-title" {{ Popper::delay(500,0)->arrow('round')->pop( $lessons->subject.' - '.$lessons->description); }}>{{ $lessons->subject.' - '.$lessons->description }}</a>
                                 <h5 class="text-start fw-bold card-text " >{{ 'Section - '.$lessons->section }}</h5>
-                                <h5 class="text-start card-text py-3">{{ $lessons->teachers->name }}</h5>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <h5 class="text-start card-text py-2">{{ $lessons->teachers->name }}</h5>
+                                    </div>
+                                </div>
                                 @if (empty($lessons->grades))
                                     <span class="text-muted">{{ __(' ') }}</span>
                                 @else
@@ -20,8 +24,8 @@
                                 @endif
                                 {{-- <p>{{ $subjects->uuid }}</p> --}}
                             </div>
-                            <div class="col-lg-4 col-md-4 col-sm-4 col-4">
-                                <img src="{{ asset('/storage/images/avatar.png')}}" class="border border-info border-3 rounded-circle img-thumbnail" alt="" height="100px" width="100px">
+                            <div class="col-lg-3 col-md-3 col-sm-3 col-3">
+                                <img src="{{ asset('/storage/images/avatar.png')}}" class="border border-info border-3 rounded-circle" alt="" height="50px" width="50px">
                             </div>
                         </div>
                     </div>
--- resources/views/teacher/home.blade.php
+++ resources/views/teacher/home.blade.php
@@ -9,13 +9,17 @@
             <div class="card shadow h-100" style="border-radius:20px;">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-lg-8 col-md-8 col-sm-8 col-8">
+                        <div class="col-lg-9 col-md-9 col-sm-9 col-9">
                             <a href="{{ route('teacher.class',$subjects->uuid) }}" class="text-start d-block text-truncate h4 fw-bolder text-dark card-title" {{ Popper::delay(500,0)->arrow('round')->pop( $subjects->subject.' - '.$subjects->description); }}>{{ $subjects->subject.' - '.$subjects->description }}</a>
                             <h5 class="text-start fw-bold card-text text-decoration-underline" >{{ 'Section - '.$subjects->section }}</h5>
-                            <h5 class="text-start card-text py-3">{{ $subjects->teacher->name }}</h5>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <h5 class="text-start card-text py-2">{{ $subjects->teacher->name }}</h5>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-lg-4 col-md-4 col-sm-4 col-4">
-                            <img src="{{ asset('/storage/images/avatar.png')}}" class="border border-info border-3 rounded-circle img-thumbnail" alt="" height="100px" width="100px">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-3">
+                            <img src="{{ asset('/storage/images/avatar.png')}}" class="border border-info border-3 rounded-circle" alt="" height="50px" width="50px">
                         </div>
                     </div>
                 </div>
